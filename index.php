09e6fb354845cae5d11b411cfe821227ac39d7ee
kconklin33 <kyle.conklin@scranton.edu>
2014-12-07 23:31:19 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php require_once( 'incl/header.php' );
+	require_once('incl/functions.php'); 
+	?>
+
+
+<!DOCTYPE html>
+<html lang="en-us">
+	<head>
+		<title>Kyle Conklin's Movie Database</title>
+	</head>
+	<body>
+<h1>Kyle Conklin's Movie Database</h1>
+<br><br>
+			
+		<h2><a href="actors.php">Actors Page</a><br></h2>
+		<h2><a href="movies.php">Moives Page</a><br></h2>
+		<h2><a href="studios.php">Studios Page</a><br></h2>
+	
+	
+	</body>
+</html>
+
+
+
+<?php require_once( 'incl/footer.php' ); ?>
