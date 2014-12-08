43a04122b5cd4648c3509facbaf53714ef82ec14
kconklin33 <kyle.conklin@scranton.edu>
2014-12-07 23:34:03 -0500

    Create studio.php

---
+++ studio.php
@@ -0,0 +1,11 @@
+<?php require_once( 'incl/functions.php' ); ?>
+<?php require_once( 'incl/header.php' ); ?>
+
+
+
+ <?php echo $studio['name']; ?></a>
+<br>
+<?php echo $studio['city'].' '. $studio['state'].' '. $studio['zip']; ?>
+
+
+<?php require_once( 'incl/footer.php' ); ?>
